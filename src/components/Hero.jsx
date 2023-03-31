97e18a447a15cd9532f3c30ff4a8d0344dde6cc3
Aaron Sanchez <saarong12@gmail.com>
2023-03-31 11:15:10 -0500

    Created nav component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
+import Nav from './Nav'
+
 const Hero = () => {
   return (
     <>
+      <Nav />
       <header className="hero">
         <h1>Silverado Media Co.</h1>
       </header>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <p>SILVERADO MEDIA CO.</p>
+        <ul>
+          <li>About</li>
+          <li>My Work</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
